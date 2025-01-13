5a3d129b6215fb0092458f890c35e707d3555776
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-01-12 22:19:02 -0800

    Update to include different plotting positions by name.

--- R/scoring_functions.R
+++ R/scoring_functions.R
@@ -8,23 +8,50 @@
 #' by spacing the uniform scores `u` by `1 / (1 - u)`.
 #'
 #' @param x Numeric vector.
-#' @param a Single numeric; adjustment value for uniform scores. See Details.
+#' @param pos Positional adjustment for uniform scores. See Details.
+#' Can be a single numeric, or could be named after one of the
+#' proponents behind a choice of the numeric: "Weibull", "Beard",
+#' "Gringorten", or "Hazen".
 #' @param na.rm Logical indicating whether `NA` and `NaN` values should be
 #' removed from the output.
 #' @details Uniform scores are calculated by `(rank+a)/(n+1+2*a)`,
-#' where `rank` is the ranked `x` values.
+#' where `rank` is the ranked `x` values, and `a` is the positional
+#' adjustment `pos`. Alternatively, could be named after an individual
+#' associated with a choice of `a`:
+#'
+#' - Weibull (1939) proposed `a = 0`.
+#' - Beard (1943) proposed `a = -0.31`.
+#' - Gringorten (1963) proposed `a = -0.44`.
+#' - Hazen (1914) proposed `a = -0.5`.
+#'
 #' @return Vector of uniform scores.
 #' @author Thanks to Dr. Harry Joe for providing a starting framework for
 #' the `uscore()` function.
 #' @examples
 #' x <- c(0.3, 0.56, NA, 0.1, -12)
 #' uscore(x)
-#' nscore(x)
+#' uscore(x, pos = "Gringorten")
+#' nscore(x, pos = -0.4)
 #' rpscore(x)
 #'
 #' @export
 #' @rdname scores
-uscore <- function(x, a = -0.5, na.rm = FALSE) {
+uscore <- function(x, pos = "Hazen", na.rm = FALSE) {
+  if (is.character(pos)) {
+    poslow <- tolower(pos)
+    a <- switch(
+      poslow,
+      weibull = 0,
+      beard = -0.31,
+      gringorten = 0.44,
+      hazen = -0.5
+    )
+    if (is.null(a)) {
+      stop("Did not recognize positional adjustment '", pos, "'.")
+    }
+  } else {
+    a <- pos
+  }
   if (na.rm) x <- x[!is.na(x)]
   nr <- sum(!is.na(x))
   us <- (seq_len(nr) + a) / (nr + 1 + 2 * a)
@@ -34,12 +61,12 @@ uscore <- function(x, a = -0.5, na.rm = FALSE) {
 
 #' @export
 #' @rdname scores
-nscore <- function(x, a = -0.5, na.rm = FALSE) {
-  stats::qnorm(uscore(x, a = a, na.rm = na.rm))
+nscore <- function(x, pos = "Hazen", na.rm = FALSE) {
+  stats::qnorm(uscore(x, pos = pos, na.rm = na.rm))
 }
 
 #' @export
 #' @rdname scores
-rpscore <- function(x, a = -0.5, na.rm = FALSE) {
-  1 / (1 - uscore(x, a = a, na.rm = na.rm))
+rpscore <- function(x, pos = "Hazen", na.rm = FALSE) {
+  1 / (1 - uscore(x, pos = pos, na.rm = na.rm))
 }
--- tests/testthat/test-scoring_functions.R
+++ tests/testthat/test-scoring_functions.R
@@ -1,17 +1,17 @@
 test_that("uscore calculates properly", {
   x <- c(0.3, 0.56, NA, NaN, 0.1, -12)
-  expect_equal(uscore(x, a = 0, na.rm = TRUE), c(0.6, 0.8, 0.4, 0.2))
-  expect_equal(uscore(x, a = 0), c(0.6, 0.8, NA, NA, 0.4, 0.2))
+  expect_equal(uscore(x, pos = 0, na.rm = TRUE), c(0.6, 0.8, 0.4, 0.2))
+  expect_equal(uscore(x, pos = 0), c(0.6, 0.8, NA, NA, 0.4, 0.2))
   x <- 1:5
-  expect_equal(uscore(x, a = 1), c(0.25, 0.375, 0.5, 0.625, 0.75))
+  expect_equal(uscore(x, pos = 1), c(0.25, 0.375, 0.5, 0.625, 0.75))
 })
 
 test_that("nscore calculates properly", {
   x <- 1:4
-  expect_equal(nscore(x, a = 0), qnorm(1:4 / 5))
+  expect_equal(nscore(x, pos = 0), qnorm(1:4 / 5))
 })
 
 test_that("rpscore calculates properly", {
   x <- 1:4
-  expect_equal(rpscore(x, a = 0), 5 / 4:1)
+  expect_equal(rpscore(x, pos = 0), 5 / 4:1)
 })
